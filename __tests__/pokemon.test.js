fed407aa5b1e52fdca0dc4826095bbdcc9b8c9d9
booayl <angelaboo226@icloud.com>
2024-03-21 16:50:43 +0000

    completed till kata6

--- __tests__/pokemon.test.js
+++ __tests__/pokemon.test.js
@@ -1,4 +1,4 @@
-const {Pokemon, Normal, Fire, Water, Grass} = require('../pokemon.js')
+const { Pokemon, Normal, Fire, Water, Grass, Charmander, Squirtle, Bulbasaur, Rattata, Pokeball, Trainer } = require('../pokemon.js')
 
 describe('catchPokemon', () => {
 
@@ -8,7 +8,7 @@ describe('catchPokemon', () => {
     const Eevee = new Pokemon('Eevee', 55, 18, 'headbutt')
     const actualOutput = (Eevee)
     // Assert
-    expect(actualOutput).toEqual({name:'Eevee', hitPoints:55, atkDmg:18, move:'headbutt'})
+    expect(actualOutput).toEqual({ name: 'Eevee', hitPoints: 55, atkDmg: 18, move: 'headbutt', type: null })
   })
 
   test('When takeDmg method is invoked, reduces hitPoints by arg amount', () => {
@@ -42,10 +42,130 @@ describe('catchPokemon', () => {
 
   test('When a new pokemon class is created using a the type constructor, add all properties of parent constructor plus type (normal)', () => {
     // Arrange
-    const expected = {name:'Eevee', hitPoints:55, atkDmg:18, move:'headbutt', type: 'normal'}
+    const expected = { name: 'Eevee', hitPoints: 55, atkDmg: 18, move: 'headbutt', type: 'Normal' }
     // Act
     const Eevee = new Normal('Eevee', 55, 18, 'headbutt')
     // Assert
     expect(Eevee).toEqual(expected)
+  })
+
+  test('test isEffectiveAgainst by passing in a new pokemon', () => {
+    const Flareon = new Fire('Flareon', 65, 20, 'Fire blast')
+    const Leafeon = new Grass('Leafeon', 65, 17, 'Giga drain')
+
+    const actual = Flareon.isEffectiveAgainst(Leafeon);
+
+    expect(actual).toBe(true);
+  })
+
+  test('charmander class extends from fire class', () => {
+    const CharChar = new Charmander('CharChar', 44, 17, 'Flamethrower')
+
+    expect(CharChar instanceof Fire).toBe(true);
+  })
+
+  test('new charmander class should overwrite move to ember ', () => {
+    const CharChar = new Charmander('CharChar', 44, 17)
+
+    expect(CharChar.move).toBe('ember');
+  })
+
+  test('new Rattata class extends from normal class ', () => {
+    const Ratta = new Rattata('Ratta', 30, 15)
+
+    expect(Ratta instanceof Normal).toBe(true);
+    expect(Ratta.move).toBe('tackle');
+  })
+
+  test('creates a new instance of Pokeball', () => {
+    const testPokeball = new Pokeball
+
+    expect(testPokeball instanceof Pokeball).toBe(true);
+  })
+
+  test('if new Pokeball is empty, should return true ', () => {
+    const testPokeball = new Pokeball
+    const actual = testPokeball.isEmpty()
+
+    expect(actual).toBe(true);
+  })
+
+  test('new Pokeball contains should return empty', () => {
+    const testPokeball = new Pokeball
+    const actual = testPokeball.contains()
+
+    expect(actual).toBe('empty ...');
+  })
+
+  test('when throw invoke, but store is empty, will return passed pokemon', () => {
+    const testPokeball = new Pokeball
+    const Eevee = new Normal('Eevee', 55, 18, 'headbutt')
+    testPokeball.throw(Eevee)
+
+    expect(testPokeball.stored).toEqual({ name: 'Eevee', hitPoints: 55, atkDmg: 18, move: 'headbutt', type: 'Normal' });
+  })
+
+  test('Pokeball contains should return pokemon name when passed pokemon', () => {
+    const testPokeball = new Pokeball
+    const Eevee = new Normal('Eevee', 55, 18, 'headbutt')
+    testPokeball.throw(Eevee)
+
+    expect(testPokeball.contains()).toBe('Eevee');
+  })
+
+  test('if no argument is pass in throw, and stored is null, should inform pokeball is empty ', () => {
+    const testPokeball = new Pokeball
+
+    expect(testPokeball.throw()).toBe('This Pokeball is empty!');
+  })
+
+  test('when argument is pass in and is empty, it catched the pokemon ', () => {
+    const testPokeball = new Pokeball
+    const Eevee = new Normal('Eevee', 55, 18, 'headbutt')
+    testPokeball.throw(Eevee)
+
+    expect(testPokeball.stored).toEqual(Eevee)
+  })
+
+  test('if no argument is pass in throw, but has a pokemon, should inform user ', () => {
+    const testPokeball = new Pokeball
+    const Eevee = new Normal('Eevee', 55, 18, 'headbutt')
+    testPokeball.throw(Eevee)
+    const actual = testPokeball.throw()
+
+    expect(actual).toEqual(Eevee);
+  })
+
+  test('Trainer got up to 6 balls to catch Pokemons', () => {
+    const Ash = new Trainer()
+    const Eevee = new Normal('Eevee', 55, 18, 'headbutt')
+    Ash.catch(Eevee)
+    console.log(Eevee)
+    const actual = Ash.belt[0].stored
+
+    expect(actual).toEqual(Eevee);
+  })
+
+  test('When belt is full, should inform user', () => {
+    const EeveeTrainer = new Trainer()
+    const Eevee = new Normal('Eevee', 55, 18, 'headbutt')
+    EeveeTrainer.catch(Eevee)
+    EeveeTrainer.catch(Eevee)
+    EeveeTrainer.catch(Eevee)
+    EeveeTrainer.catch(Eevee)
+    EeveeTrainer.catch(Eevee)
+    EeveeTrainer.catch(Eevee)
+    const actual = EeveeTrainer.catch(Eevee)
+
+    expect(actual).toEqual('No more Pokeballs :(');
+  })
+
+  test('invoke getPokemon and should return via throw pokemon', () => {
+    const EeveeTrainer = new Trainer()
+    const Eevee = new Normal('Eevee', 55, 18, 'headbutt')
+    EeveeTrainer.catch(Eevee)
+    const actual = EeveeTrainer.getPokemon('Eevee')
+
+    expect(actual).toEqual(Eevee);
   })
 });
--- pokemon.js
+++ pokemon.js
@@ -1,5 +1,5 @@
 class Pokemon {
-  constructor(name,hitPoints, atkDmg, move = 'tackle') {
+  constructor(name, hitPoints, atkDmg, move = 'tackle') {
     this.name = name
     this.hitPoints = hitPoints
     this.atkDmg = atkDmg
@@ -21,26 +21,26 @@ class Pokemon {
       return true
     } else {
       return false
-  }
+    }
 
   }
 }
 
 class Normal extends Pokemon {
-    constructor(name, hitPoints, atkDmg, move, type) {
-      super(name, hitPoints, atkDmg, move, type)
-      this.type = 'normal'
-    }
+  constructor(name, hitPoints, atkDmg, move, type) {
+    super(name, hitPoints, atkDmg, move, type)
+    this.type = 'Normal'
   }
+}
 
 class Fire extends Pokemon {
   constructor(name, hitPoints, atkDmg, move, type) {
     super(name, hitPoints, atkDmg, move, type)
-    this.type = 'fire'
+    this.type = 'Fire'
   }
 
   isEffectiveAgainst(opponent) {
-    if (opponent.type === 'grass') {
+    if (opponent.type === 'Grass') {
       return true
     } else {
       return false
@@ -48,7 +48,7 @@ class Fire extends Pokemon {
   }
 
   isWeakTo(opponent) {
-    if (opponent.type === 'water') {
+    if (opponent.type === 'Water') {
       return true
     } else {
       return false
@@ -56,6 +56,134 @@ class Fire extends Pokemon {
   }
 }
 
+class Grass extends Pokemon {
+  constructor(name, hitPoints, atkDmg, move, type) {
+    super(name, hitPoints, atkDmg, move, type)
+    this.type = 'Grass'
+  }
+
+  isEffectiveAgainst(opponent) {
+    if (opponent.type === 'Water') {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  isWeakTo(opponent) {
+    if (opponent.type === 'Fire') {
+      return true
+    } else {
+      return false
+    }
+  }
+}
+
+class Water extends Pokemon {
+  constructor(name, hitPoints, atkDmg, move, type) {
+    super(name, hitPoints, atkDmg, move, type)
+    this.type = 'Water'
+  }
+
+  isEffectiveAgainst(opponent) {
+    if (opponent.type === 'Fire') {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  isWeakTo(opponent) {
+    if (opponent.type === 'Grass') {
+      return true
+    } else {
+      return false
+    }
+  }
+}
+
+class Charmander extends Fire {
+  constructor(name, hitPoints, atkDmg, move, type) {
+    super(name, hitPoints, atkDmg, 'ember', type)
+  }
+}
+
+class Squirtle extends Water {
+  constructor(name, hitPoints, atkDmg, move, type) {
+    super(name, hitPoints, atkDmg, 'water gun', type)
+  }
+}
+
+class Bulbasaur extends Grass {
+  constructor(name, hitPoints, atkDmg, move, type) {
+    super(name, hitPoints, atkDmg, 'vine whip', type)
+  }
+}
+
+class Rattata extends Normal {
+  constructor(name, hitPoints, atkDmg, move, type) {
+    super(name, hitPoints, atkDmg, move, type)
+  }
+}
+
+class Pokeball {
+  constructor() {
+    this.stored = null
+  }
+
+  throw(newPokemon) {
+
+    if (!newPokemon) {
+      if (this.stored === null) {
+        return 'This Pokeball is empty!'
+      } else {
+        console.log(`GO ${this.stored.name}!!`);
+        return this.stored;
+      }
+    } else {
+      if (this.stored === null) {
+        this.stored = newPokemon;
+        console.log(`you caught ${this.stored.name}`)
+        return this.stored;
+      }
+    }
+  }
+
+  contains() {
+    if (this.stored === null) {
+      return 'empty ...'
+    } else { return this.stored.name }
+  }
+
+  isEmpty() {
+    if (this.stored === null) {
+      return true;
+    } else { return false; }
+  }
+}
+
+class Trainer {
+  constructor() {
+    this.belt = [new Pokeball(), new Pokeball(), new Pokeball(), new Pokeball(), new Pokeball(), new Pokeball()]
+  }
+
+  catch(pokemon) {
+    for (let i = 0; i < this.belt.length; i++) {
+      if (this.belt[i].stored === null) {
+        this.belt[i].throw(pokemon);
+        this.belt[i].stored = pokemon;
+        break;
+      } else { return 'No more Pokeballs :(' }
+    }
+  }
+
+  getPokemon(pokemonName) {
+    for (let ball of this.belt) {
+      if (ball.stored.name === pokemonName)
+        return ball.throw()
+    }
+  }
+}
 
 
-module.exports = {Pokemon, Normal, Fire, Water, Grass}
+module.exports = { Pokemon, Normal, Fire, Water, Grass, Charmander, Squirtle, Bulbasaur, Rattata, Pokeball, Trainer }
